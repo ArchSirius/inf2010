2e384f7ec53c0304ae08f5de7d5283bc5f29dc03
Pacome Bondet de La Bernardie <pabondet@gmail.com>
2015-02-05 16:14:17 -0500

    Double stack presque fini

--- tp2/DoubleStackQueue.java
+++ tp2/DoubleStackQueue.java
@@ -4,4 +4,34 @@ import java.util.EmptyStackException;
 public class DoubleStackQueue<Elem> implements myQueue<Elem>
 {
    //A completer
+    
+    private Stack<Elem> stack1;
+    private Stack<Elem> stack2;
+    
+    public boolean empty()
+    {
+        return stack1.size() == 0;
+    }
+    
+    public void push(Elem elem)
+    {
+        stack1.push(elem);
+    }
+    
+    public Elem pop(){
+        if (stack1.size() == 0) {
+            return null;
+        }
+        for (int i = 0; i < stack1.size(); i++) {
+            stack2.push(stack1.pop());
+        }
+
+        Elem temp = stack2.pop(); 
+
+        for (int i = 0; i < stack2.size(); i++) {
+            stack1.push(stack2.pop());
+        }
+        return temp;
+    }
+        
 }
