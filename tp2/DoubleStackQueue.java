103e9778fbe6bf227f6928a9bed71fffd3e5c4f2
Samuel Rondeau <samuel.rondeau@polymtl.ca>
2015-02-07 21:30:29 -0500

    Tout termine avec DoubleStackQueue correct

--- tp2/DoubleStackQueue.java
+++ tp2/DoubleStackQueue.java
@@ -3,35 +3,40 @@ import java.util.EmptyStackException;
 
 public class DoubleStackQueue<Elem> implements myQueue<Elem>
 {
-   //A completer
+    private Stack<Elem> stack1 = new Stack<Elem>(); // stack principal
+    private Stack<Elem> stack2 = new Stack<Elem>(); // stack de dépilement
     
-    private Stack<Elem> stack1;
-    private Stack<Elem> stack2;
-    
-    public boolean empty()
-    {
-        return stack1.size() == 0;
-    }
-    
-    public void push(Elem elem)
-    {
-        stack1.push(elem);
-    }
-    
-    public Elem pop(){
-        if (stack1.size() == 0) {
-            return null;
+    public void offer(Elem element){
+        if (stack1.empty()){
+            stack1.push(element);
+        } else {
+            stack2.push(stack1.pop());  // changement de stack
+            offer(element);             // appel récursif
+            stack1.push(stack2.pop());  // retour au stack1
         }
-        for (int i = 0; i < stack1.size(); i++) {
-            stack2.push(stack1.pop());
-        }
+    }
 
-        Elem temp = stack2.pop(); 
-
-        for (int i = 0; i < stack2.size(); i++) {
-            stack1.push(stack2.pop());
+    public Elem peek(){
+        Elem element;
+        try{
+            element = stack1.peek();
+        } catch(EmptyStackException e){
+            element = null;
         }
-        return temp;
+        return element;
     }
-        
+
+    public Elem poll(){
+        Elem element;
+        try{
+            element = stack1.pop();
+        } catch(EmptyStackException e){
+            element = null;
+        }
+        return element;
+    }
+
+    public boolean empty(){
+        return stack1.empty();
+    }
 }
